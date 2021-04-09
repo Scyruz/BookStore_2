6974c428c8320ad9b9232ccd09a04071c7f5b8fa
Priscila Ruiz <A01561869@itesm.mx>
2021-04-08 21:28:27 -0700

    parte 1

--- BookStore_2/ViewController.swift
+++ BookStore_2/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var myTableView: UITableView!
+    var managedObjectContext: NSManagedObjectContext!
+    
+    @IBAction func addNew(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        managedObjectContext = appDelegate.persistentContainer.viewContext as NSManagedObjectContext
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
 
